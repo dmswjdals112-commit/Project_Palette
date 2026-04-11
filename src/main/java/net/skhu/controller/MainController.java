14f5509837492f932046b0a73005a146baa9a4ee
dmswjdals112-commit <dmswjdals112@gmail.com>
2026-04-11 23:11:37 +0900

    직접 구현 / MainController 수정 mainPage,searchPage,loginPage,signUpPage, streamPage,upLoadPage,editPage get요청 액션 메소드 작성

--- src/main/java/net/skhu/controller/MainController.java
+++ src/main/java/net/skhu/controller/MainController.java
@@ -14,8 +14,55 @@ import lombok.extern.slf4j.Slf4j;
 public class MainController {
 	private ModelMapper modelMapper = new ModelMapper();
 	
-	@GetMapping("list")
+	// mainPage, searchPage--------------------------------------------------
+	// get요청, mainPage.html로 이동
+	@GetMapping("mainPage")
 	public String mainPage(Model model) {
 		return "palette/mainPage";
 	}
+	// get요청, searchPage.html로 이동
+	@GetMapping("searchPage")
+	public String searchPage(Model model) {
+		return "palette/searchPage";
+	}
+	// -----------------------------------------------------------------------
+	
+	
+	// loginPage, signUpPage--------------------------------------------------
+	// get요청, loginPage.html로 이동
+	@GetMapping("loginPage")
+	public String loginPage(Model model) {
+		return "palette/loginPage";
+	}
+	
+	// get요청, signUpPage.html로 이동
+	@GetMapping("signUpPage")
+	public String signUpPage(Model model) {
+		return "palette/signUpPage";
+	}
+	// -----------------------------------------------------------------------
+	
+	
+	// streamPage-------------------------------------------------------------
+	// get요청, streamPage.html로 이동
+	@GetMapping("streamPage")
+	public String streamPage(Model model) {
+		return "palette/streamPage";
+	}
+	// -----------------------------------------------------------------------
+	
+	
+	// upLoadPage, editPage---------------------------------------------------
+	// get요청, upLoadPage.html로 이동
+	@GetMapping("upLoadPage")
+		public String upLoadPage(Model model) {
+		return "palette/upLoadPage";
+	}
+	// get요청, editPage.html로 이동
+	@GetMapping("editPage")
+	public String editPage(Model model) {
+		return "palette/editPage";
+	}
+	// -----------------------------------------------------------------------
+	
 }
